94b2a5f80b4bfaeb6f99b2ddd6ba956c31f786ee
churchrat <mihailkrustev08@gmail.com>
2024-03-26 23:09:26 +0200

    commit

---
+++ Hideout/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Hideout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string map = Console.ReadLine();
+            bool foundHideout = false;
+            int index = -1;
+            int count = 0;
+            int minimumCount = 0;
+            while (foundHideout == false)
+            {
+                string[] input = Console.ReadLine().Split();
+                char searchedCharacter = char.Parse(input[0]);
+                minimumCount = int.Parse(input[1]);
+
+                for (int i = 0; i < map.Length; i++)
+                {
+                    if (map[i] == searchedCharacter)
+                    {
+                        if (count == 0)
+                        {
+                            index = i;
+                        }
+                        count++;
+                        if (count >= minimumCount)
+                        {
+                            foundHideout = true;
+                            minimumCount = count;
+                        }
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+            }
+            Console.WriteLine($"Hideout found at index {index} and it is with size {minimumCount}!");
+        }
+    }
+}
---
+++ ShakeIt/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ShakeIt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = Console.ReadLine();
+            int i = input.IndexOf(pattern);
+            string result = null;
+            int j = 0;
+            while (j < pattern.Length)
+            {
+                if (input.Contains(pattern))
+                {
+                    Console.WriteLine("Shake it");
+
+                    while (input.Contains(pattern))
+                    {
+                        i = input.LastIndexOf(pattern);
+                        input = input.Remove(i, pattern.Length);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No shake");
+                    break;
+                }
+                j++;
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
